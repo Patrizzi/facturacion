ab03d2113424e5ddb889d0e7305b0be70db06c33
Fernando <fernandomv.0102@gmail.com>
2021-11-29 09:56:07 -0500

    nuevas Ramas

---
+++ database/migrations/2021_11_29_094850_create_nota_venta_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotaVentaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nota_venta', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nota_venta');
+    }
+}
