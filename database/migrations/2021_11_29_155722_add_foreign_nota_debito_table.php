5ec920563fbaf3e161d612c13e971113e5b11b1c
Chrisopher <christojhg@gmail.com>
2021-11-30 10:51:45 -0500

    migraciones de nota de debito agregado

--- database/migrations/2021_11_29_155722_add_foreign_nota_debito_table.php
+++ database/migrations/2021_11_29_155722_add_foreign_nota_debito_table.php
@@ -13,7 +13,21 @@ class AddForeignNotaDebitoTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('nota_debito', function (Blueprint $table) {
+            $table->string('codigo_n_d')->nullable()->after('id');
+            $table->unsignedBigInteger('facturacion_id')->nullable()->after('codigo_n_d');
+            $table->foreign('facturacion_id')->references('id')->on('facturacion')->onDelete('cascade');
+            $table->unsignedBigInteger('boleta_id')->nullable()->after('facturacion_id');
+            $table->foreign('boleta_id')->references('id')->on('boleta')->onDelete('cascade');
+            $table->string('tipo')->nullable()->after('boleta_id');
+            $table->unsignedBigInteger('almacen_id')->nullable()->after('tipo');
+            $table->foreign('almacen_id')->references('id')->on('almacen')->onDelete('cascade');
+            $table->string('op_gravada')->default('0')->after('almacen_id');
+            $table->string('op_inafecta')->default('0')->after('op_gravada');
+            $table->string('op_exonerada')->default('0')->after('op_inafecta');
+            $table->string('op_gratuita')->default('0')->after('op_exonerada');
+            $table->string('motivo')->after('op_gratuita');
+        });
     }
 
     /**
@@ -23,6 +37,8 @@ class AddForeignNotaDebitoTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('nota_debito', function (Blueprint $table) {
+            $table->dropColumn('codigo_n_d');
+        });
     }
 }
--- database/migrations/2021_11_29_155727_add_foreign_nota_debito_registro_table.php
+++ database/migrations/2021_11_29_155727_add_foreign_nota_debito_registro_table.php
@@ -13,7 +13,16 @@ class AddForeignNotaDebitoRegistroTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('nota_debito_registro', function (Blueprint $table) {
+            $table->unsignedBigInteger('nota_debito_id')->after('id');
+            $table->foreign('nota_debito_id')->references('id')->on('nota_debito')->onDelete('cascade');
+            $table->unsignedBigInteger('producto_id')->nullable()->after('nota_debito_id');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('cascade');
+            $table->unsignedBigInteger('servicio_id')->nullable()->after('producto_id');
+            $table->foreign('servicio_id')->references('id')->on('servicios')->onDelete('cascade')->after('id');
+            $table->double('precio',17,2)->after('servicio_id');
+            $table->integer('cantidad')->after('precio');
+        });
     }
 
     /**
@@ -23,6 +32,8 @@ class AddForeignNotaDebitoRegistroTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('nota_debito', function (Blueprint $table) {
+            $table->dropColumn('nota_debito_id');
+        });
     }
 }
