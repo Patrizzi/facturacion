480d5e8ee70c75cd0404d1190893cfcd72558159
Chrisopher <christojhg@gmail.com>
2022-02-17 17:31:49 -0500

    facturacion manual 100%

--- resources/views/transaccion/venta/facturacion/facturacion_manual/pdf.blade.php
+++ resources/views/transaccion/venta/facturacion/facturacion_manual/pdf.blade.php
@@ -189,33 +189,7 @@
 
 <!-- Fin Totales de Productos -->
 <br>
-<table style="border-collapse: separate;">
-    <tr>
-       <th style="width: 2%;border-color: white"></th>
-       @foreach($banco as $bancos)
-       @if($banco_count==3)
-       <th width="33%" style="border: 1px #808080 solid;border-radius: 8px;">
-        @elseif($banco_count==2)
-        <th width="50%"style="border: 1px #808080 solid;border-radius: 8px;">
-            @elseif($banco_count==1)
-            <th width="100%"style="border: 1px #808080 solid;border-radius: 8px;">
-                @else
-                <th width="20%"style="border: 1px #808080 solid;border-radius: 8px;">
-                    @endif
-                    <img  src="{{asset('img/logos/'.$bancos->foto)}}" style="height: 30px;"><br>
-                    <span style="font-size: 11px"><strong> {{$bancos->tipo_cuenta}}</strong></span>
-                    <br>
-                    <span style="font-size: 12px">
-                      S/: {{$bancos->numero_soles}}
-                      <br>
-                      $: {{$bancos->numero_dolares}}<br>
-                  </span>
-              </p>
-          </th>
-          <th style="width: 2%;border-color: white"></th>
-          @endforeach
-      </tr>
-  </table>
+@include('layout_bancos_pdf')
   <div class="row">
     <br>
 
