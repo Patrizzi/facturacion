dbf0028fb3ba005d9f18bae38f273324dd58eedd
Chrisopher <christojhg@gmail.com>
2022-03-03 09:33:38 -0500

    creacion de duplicados para cotizaciones

--- resources/views/transaccion/venta/cotizacion/factura/create2.blade.php
+++ resources/views/transaccion/venta/cotizacion/factura/create2.blade.php
@@ -491,11 +491,12 @@
                 },
                 processResults: function (data) {
                     //validador de articulos multiples
+                    let data_length = data.length;
                     let articles_selected_ajax = document.getElementsByClassName("select2_demo_3");
                     let articles_selected_count_ajax = articles_selected_ajax.length; 
                     for(var z=0;z<articles_selected_count_ajax;z++){
                         var selected_ajax=document.getElementsByClassName("select2_demo_3 select_change")[z].value;
-                        for(var y=0;y<10;y++){
+                        for(var y=0;y<data_length;y++){
                             if(selected_ajax == data[y].id+ " | " + data[y].codigo + " | " + data[y].codigo_original + " | " + data[y].nombre){
                                 data[y].disabled=true;
                             }
