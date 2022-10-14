7218cad01a0669b92e93a125ea7e4355bd6d6ae4
DanielRomanB <danielrberru@gmail.com>
2022-10-14 13:35:24 -0500

    fecha pago facturar cotizacion manual

--- resources/views/transaccion/venta/cotizacion/manual/facturar.blade.php
+++ resources/views/transaccion/venta/cotizacion/manual/facturar.blade.php
@@ -201,55 +201,56 @@
                 </div>
             </div>
         </div>
+        <!-- Modal -->
+        <div class="modal fade bd-example-modal-lg" id="cuotas_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+            <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Registrar cuotas</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert"   id="alert_campos" style="display: none">
+                    <strong style="font-size:11px">Rellenar todos los campos</strong>
+                    <button type="button" class="close_model_rc close" onclick="cerrar_but_rc()" style="padding: 6;">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert"  id="suma_campos" style="display: none" >
+                    <strong style="font-size:11px">La suma de las cuotas exceden el monto total</strong>
+                    <button type="button" class="close_model_mt close" onclick="cerrar_but_mt()" style="padding: 6;">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="row_number">
+                    <div class="pago_modal row">
+                        <div class="col-sm-1"><label>Fecha:</label></div>
+                        <div class="col-sm-4">
+                            <input type="date" name="fecha_pago[]" id="fecha_pago0"  class="fecha_pago form-control" >
+                        </div>
+                        <div class="col-sm-1"><label>Monto:</label></div>
+                        <div class="col-sm-4">
+                            <div class="input-group mb-3" style="padding-right:15px">
+                                <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon3">{{$cotizacion->moneda->simbolo}}</span>
+                                </div>
+                                <input type="text" name="monto_pago[]" id="monto_pago0" class="monto_pago form-control"   >
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <label ><button type="button"  aria-hidden="true" id="add_pago" class="add_pago btn btn-success"><i class="fa fa-plus-square-o fa-lg" > </i></button></label>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
     </form>
 </div>
-<!-- Modal -->
-<div class="modal fade bd-example-modal-lg" id="cuotas_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
-    <div class="modal-dialog modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Registrar cuotas</h5>
-           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-          <div class="alert alert-danger alert-dismissible fade show" role="alert"   id="alert_campos" style="display: none">
-            <strong style="font-size:11px">Rellenar todos los campos</strong>
-            <button type="button" class="close_model_rc close" onclick="cerrar_but_rc()" style="padding: 6;">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="alert alert-danger alert-dismissible fade show" role="alert"  id="suma_campos" style="display: none" >
-            <strong style="font-size:11px">La suma de las cuotas exceden el monto total</strong>
-            <button type="button" class="close_model_mt close" onclick="cerrar_but_mt()" style="padding: 6;">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="row_number">
-              <div class="pago_modal row">
-                  <div class="col-sm-1"><label>Fecha:</label></div>
-                  <div class="col-sm-4">
-                      <input type="date" name="fecha_pago[]" id="fecha_pago0"  class="fecha_pago form-control" >
-                  </div>
-                  <div class="col-sm-1"><label>Monto:</label></div>
-                  <div class="col-sm-4">
-                      <div class="input-group mb-3" style="padding-right:15px">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text" id="basic-addon3">{{$cotizacion->moneda->simbolo}}</span>
-                        </div>
-                        <input type="text" name="monto_pago[]" id="monto_pago0" class="monto_pago form-control"   >
-                      </div>
-                  </div>
-                  <div class="col-sm-2">
-                      <label ><button type="button"  aria-hidden="true" id="add_pago" class="add_pago btn btn-success"><i class="fa fa-plus-square-o fa-lg" > </i></button></label>
-              </div>
-              </div>
-          </div>
-        </div>
-      </div>
-    </div>
-</div>
+
 
 <style type="text/css">
     .ruc{border-radius: 10px; height: 150px;}
