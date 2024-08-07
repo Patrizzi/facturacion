059da3ba869de8f401d0d5496018c7dfc07e2175
JYPSAC <julioflores@jypsac.com>
2024-08-07 14:42:05 -0500

    guia remision jyp cambios

--- app/Http/Controllers/GuiaRemisionManualController.php
+++ app/Http/Controllers/GuiaRemisionManualController.php
@@ -55,12 +55,15 @@ class GuiaRemisionManualController extends Controller
     public function create()
     {
         //* GUIA REMISION MANUAL  COD GUIA
-        $cod_guia = Codigo_guia_almacen::where('serie_remision_m', 0)->first();
-        if(isset($cod_guia)){
-            $almacen_update = Codigo_guia_almacen::find($cod_guia->id);
-            $almacen_update->serie_remision_m = 1;
-            $almacen_update->cod_remision_m = 0;
-            $almacen_update->save();
+        $count_guias=GuiaRemisionManual::where('almacen_id',1)->count();
+        if ( $count_guias == 0) {
+            $cod_guia = Codigo_guia_almacen::where('serie_remision_m', 0)->first();
+            if(isset($cod_guia)){
+                $almacen_update = Codigo_guia_almacen::find($cod_guia->id);
+                $almacen_update->serie_remision_m = 1;
+                $almacen_update->cod_remision_m = 0;
+                $almacen_update->save();
+            }
         }
         // return $cod_guia;
 
