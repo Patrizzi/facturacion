36ee6c5e3ef1a756e51a84014f857bbef08630d4
Anthony Condori <alessandro_condori_2005@hotmail.com>
2024-10-01 19:02:50 -0500

    fix: :sparkles: Store function

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
+use Auth;
 
 class CommentController extends Controller
 {
@@ -10,8 +12,36 @@ class CommentController extends Controller
         return "desde el controller del create";
     }
 
-    public function Store(){
-        return "desde el controller del store";
+    public function store(Request $request) {   
+        $request->validate([
+            'contenido' => 'required|string',
+        ]);
+    
+        $task = Task::find($request->task);
+        
+        if (!$task) {
+            return redirect()->back()->withErrors(['error' => 'Tarea no encontrada']);
+        }
+    
+        $comment = new Comment([
+            'tarea_id' => $task->id,
+            'user_id' => Auth::id(),
+            'contenido' => $request->get('contenido')
+        ]);
+    
+        if ($request->hasfile('foto')){
+            $image1 = $request->file('foto');
+            $name = time() . $image1->getClientOriginalName();
+            $destinationPath = public_path('/archivos/imagenes/project_manager/');
+            $image1->move($destinationPath, $name);
+        } else {
+            $name = null;
+        }
+    
+        $comment->foto = $name;
+        $comment->save();
+    
+        return redirect()->route('project_managers.cards', $task->activity->project_manager);
     }
 
     public function Update(){
