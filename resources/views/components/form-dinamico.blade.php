76ce3952d73420bce82b3176fc85ee53790d56cd
Redmaen <redmaen19@gmail.com>
2024-10-04 10:42:20 -0500

    revert: :lipstick: Removing component formDinamico

--- resources/views/components/form-dinamico.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-cell">
-    <label for="{{$name}}">{{$label}}</label>
-    @if ($type=='text' or $type=='datetime-local')
-        <input type="{{$type}}" placeholder="{{$placeholder}}" name="{{$name}}" value="{{$value}}" required>
-    @elseif($type=='number')
-        <input type="{{$type}}" placeholder="{{$placeholder}}" name="{{$name}}" value="{{$value}}" step="1" min="1" required>
-    @endif
-</div>
