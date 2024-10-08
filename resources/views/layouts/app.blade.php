d4dde909ab8092f6c4be37aac33ff784868530b3
Jeremi_aron <chancanjeremiaron@gmail.com>
2024-10-08 17:36:12 -0500

    style: :art: Format app.css and restructure layout styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,11 @@
     <title>@yield('title', 'Titulo')</title>
     @php
         $config = auth()->user()->config;
-        $fontFamily = $config->letra !== 'none' ? $config->letra : null;
-        $fontSize = !empty($config->tamano_letra) ? $config->tamano_letra : null;
-        $fontSizePerfil = !empty($config->tamano_letra_perfil) ? $config->tamano_letra_perfil : null;
     @endphp
+
     <style>
-        :root{
+        :root {
             --config-color-nombre: {{ $config->color_nombre }};
-            --config-font-size-perfil: {{ $fontSizePerfil }};
             --config-color-sombra-nombre: {{ $config->color_sombra_nombre }};
             --config-fondo-perfil: url("{{ asset('/css/patterns/') . '/' . $config->fondo_perfil }}");
             --config-color-border-foto: {{ $config->color_borde_foto }};
@@ -22,21 +19,26 @@
         }
 
         body {
-            @if($fontFamily) font-family: {{ $fontFamily }}; @endif
-            @if($fontSize) font-size: {{ $fontSize }}; @endif
+            font-family: {{ $config->letra !== 'none' ? $config->letra : 'inherit' }};
+            font-size: {{ !empty($config->tamano_letra) ? $config->tamano_letra : 'inherit' }};
         }
 
-        .form-control, table, span {
-            @if($fontFamily) font-family: {{ $fontFamily }}; @endif
-            @if($fontSize) font-size: {{ $fontSize }}; @endif
+        .form-control,
+        table,
+        span {
+            font-family: {{ $config->letra !== 'none' ? $config->letra : 'inherit' }};
+            font-size: {{ !empty($config->tamano_letra) ? $config->tamano_letra : 'inherit' }};
         }
 
         span.select2-selection__placeholder {
-            font-size: @if(!$fontSize || $fontSize == 'smaller') small; @else {{ $fontSize }}; @endif
+            font-size: {{ !$config->tamano_letra || $config->tamano_letra == 'smaller' ? 'small' : $config->tamano_letra }};
+        }
+        li.nav-header > div.dropdown.profile-element > a > span:nth-child(3) {
+            font-size: {{ $config->tamano_letra_perfil ?? "12px" }}
         }
     </style>
+
     {!! push_asset_once([
-        asset('css/plugins/ladda/ladda-themeless.min.css'),
         asset('css/bootstrap.min.css'),
         asset('font-awesome/css/font-awesome.css'),
         asset('css/animate.css'),
@@ -48,12 +50,12 @@
         asset('css/plugins/switchery/switchery.css'),
         asset('css/plugins/select2/select2.min.css'),
         asset('css/plugins/daterangepicker/daterangepicker-bs3.css'),
-        asset('main.css'),
         asset('css/plugins/toastr/toastr.min.css'),
         asset('css/layout/app.css'),
+        asset('css/plugins/ladda/ladda-themeless.min.css'),
+        asset('main.css'),
     ]) !!}
     @stack('css')
-    
 </head>
 
 <body>
@@ -72,10 +74,10 @@
         asset('js/plugins/metisMenu/jquery.metisMenu.js'),
         asset('js/plugins/slimscroll/jquery.slimscroll.min.js'),
         asset('js/inspinia.js'),
-        asset('js/plugins/pace/pace.min.js'),
-        asset('js/plugins/ladda/spin.min.js'),
         asset('js/plugins/ladda/ladda.min.js'),
         asset('js/plugins/ladda/ladda.jquery.min.js'),
+        asset('js/plugins/pace/pace.min.js'),
+        asset('js/plugins/ladda/spin.min.js'),
         asset('js/plugins/toastr/toastr.min.js'),
     ]) !!}
     @stack('js')
