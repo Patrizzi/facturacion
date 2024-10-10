2dfedb0eb31d703e29269adaa81f0f12e02885a5
Jeremi_aron <chancanjeremiaron@gmail.com>
2024-10-10 18:49:39 -0500

    feat: :lipstick: Add PieChart component

---
+++ resources/views/components/pie-chart.blade.php
@@ -0,0 +1,33 @@
+<div class="ibox">
+    <div class="ibox-title">
+        <h3>{{ $title }}</h3>
+    </div>
+    <div class="ibox-content">
+        <canvas id="doughnutChart"></canvas>
+    </div>
+</div>
+@push('js')
+    {!! push_asset_once('js/plugins/chartJs/Chart.min.js') !!}
+    <script>
+        $(function() {
+            var doughnutData = {
+                labels: @json($data['labels']),
+                datasets: [{
+                    data: @json($data['values']),
+                    backgroundColor: @json($data['colors'])
+                }]
+            };
+
+            var doughnutOptions = {
+                responsive: true
+            };
+
+            var ctx4 = document.getElementById("doughnutChart").getContext("2d");
+            new Chart(ctx4, {
+                type: 'doughnut',
+                data: doughnutData,
+                options: doughnutOptions
+            });
+        });
+    </script>
+@endpush
