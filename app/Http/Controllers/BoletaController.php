7c0d8f0b4ed231b337dfb113acd6c251b724942a
JYPSAC <julioflores@jypsac.com>
2024-10-29 13:31:25 -0500

    error en comprobantes

--- app/Http/Controllers/BoletaController.php
+++ app/Http/Controllers/BoletaController.php
@@ -867,7 +867,9 @@ return view('transaccion.venta.boleta.create_ms',compact('productos','forma_pago
         $boleta_registro->save();
     }
 }
-Boleta::revision_cuotas($boleta->id);
+if($boleta->forma_pago_id == 2){
+    Boleta::revision_cuotas($boleta->id);
+}
 Kardex_entrada_registro::stock_producto_precio();
 }else {
     return redirect()->route('boleta.create')->with('campo', 'Falto introducir un campo de la tabla productos');
