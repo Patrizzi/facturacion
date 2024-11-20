6b9ad7741ddc701c13bd50ad8e40b176ae17708f
JYPSAC <julioflores@jypsac.com>
2024-11-20 14:34:11 -0500

    comprobantes inicio

---
+++ app/Http/Controllers/ComprobantesVentas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComprobantesVentas extends Controller
+{
+    public function index() {}
+
+    public function comprobantes_tabs(){
+
+    }
+    public function boleta_registers(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -567,8 +567,8 @@ Route::group(
 		Route::get('/creditos', 'CreditosAdelantosController@creditos')->name('cobranzas.creditos');
 		Route::get('/creditos_show/{id}','CreditosAdelantosController@creditos_show')->name('cobranzas.creditos_show');
 		Route::get('/creditos_show_FoB/{id}','CreditosAdelantosController@creditos_show_FoB')->name('cobranzas.creditos_show_FoB');
-
-
+	// COMPROBANTES (NEW VISTAS V2.1)
+		// Route::get('/comprantes/boleta','')
 	});
 
 Auth::routes([
