32c8412bb0c01cc6bd9cb95529a9617be8853a1c
JYPSAC <julioflores@jypsac.com>
2025-02-04 17:09:31 -0500

    v4

--- resources/views/facturacion_electronica/factura/shared/tabs.blade.php
+++ resources/views/facturacion_electronica/factura/shared/tabs.blade.php
@@ -1,28 +1,29 @@
-<ul class="nav nav-tabs" role="tablist">
-    <li>
+
+<div class="nav">
+    <li class="nav-item">
         <a class="nav-link" href="{{ route('facturacion_electronica.facturas_enviadas_list') }}" id="tab_factura"><span style="color: green;">&#9632; </span>
             Facturas
             {{-- link del tab 1 --}}
         </a>
     </li>
-    <li>
+    <li class="nav-item">
         <a class="nav-link" href="{{ route('facturacion_electronica.facturas_enviadas_list') }}" id="tab_fact_env"><span
                 style="color: orange;">&#9632;</span> Enviados
             {{-- link del tab 2 --}}
         </a>
     </li>
-    <li>
+    <li class="nav-item">
         <a class="nav-link" href="#tab-7"><span style="color: rgb(0, 255, 72);">&#9632;</span>
             Factura Manual
             {{-- link del tab 2 --}}
         </a>
     </li>
-    <li>
+    <li class="nav-item">
         <a class="nav-link" href="#tab-8"><span style="color: red;">&#9632;</span> Enviados
-            {{-- link del tab 2 --}}
+            {{-- link del tab 2 --}} 
         </a>
     </li>
-    <li>
+    <li class="nav-item">
         <a class="nav-link" href="#tab-9"><span style="color: rgb(14, 14, 194);">&#9632;</span>
             Detracciones
             {{-- link del tab 2 --}}
