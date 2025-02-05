6b64ae58e601553b3007f4dfca299b01014d149f
Coolmesito <msamaniegocalderon@gmail.com>
2025-02-04 20:54:44 -0500

    Avance del tab1

--- routes/web.php
+++ routes/web.php
@@ -586,7 +586,7 @@ Route::get("/boleta3","BOLETACONTROLLER@index3")->name('boleta3');
 //Kardex
 Route::get("/inventario2","INVENTARIOINICIALCONTROLLER@index2")->name('inventario2');
 
-Route::get('/inventario/consultas', 'PeriodoConsultaController@index2')->name('inventario.consultas');
+Route::get("/inventario/consultas", 'PeriodoConsultaController@index2')->name('inventario.consultas');
 
 
 
