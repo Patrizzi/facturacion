5be26995b382a58c5933c53bd07dd2836968eda8
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-02-05 08:33:45 -0500

    fix: :bug: Add changes requested for new project manager

--- resources/views/project_manager/create.blade.php
+++ resources/views/project_manager/create.blade.php
@@ -1,37 +1,32 @@
 @extends('layouts.app')
 @push('css')
     @once
-    <link rel="stylesheet" href="{{ asset('/css/project_managers/formDinamico.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/project_managers/formDinamico.css') }}">
     @endonce
-@endpush 
+@endpush
 @section('content')
     @if ($errors->any())
         <h3 class="alert alert-danger">{{ $errors->first() }}</h3>
     @endif
     <div class="formEnvio">
-        
+
         <div class="inbox-title">
             <h3>Crear Nuevo Proyecto</h3>
         </div>
         <div class="inbox-content">
-            {{ html()->form('POST',route('project_managers.store'))->open() }}
-                <div class="container-fluid">
-                    <div class="row ">
-                        @include('project_manager.formDinamico')
-                        <div class="w-50 p-1">
-                            {{html()->button('Crear')->class('btn btn-primary float-right m-t-n-xs ')}}
-                        </div>
-                        <div class="w-65 p-1">
-                            {{html()->a('cancelar')->text('Cancelar')->href('project_manager/index')->class('btn m-t-n-xs border border-primary')}}
-                        </div>
-                    </div>
+            {{ html()->form('POST', route('project_managers.store'))->open() }}
+            <div class="container-fluid">
+                <div class="row m-t-md">
+                    @include('project_manager.formDinamico')
+                    {{ html()->a('cancelar')->text('Cancelar')->href('project_manager/index')->class('btn btn-white btn-sm m-l-sm') }}
+                    {{ html()->button('Crear')->class('btn btn-primary m-l-sm') }}
                 </div>
+            </div>
             {{ html()->form()->close() }}
         </div>
     </div>
 @endsection
 @push('js')
-@once
-    <script>
-    </script>
-@endonce
+    @once
+        <script></script>
+    @endonce
--- resources/views/project_manager/formDinamico.blade.php
+++ resources/views/project_manager/formDinamico.blade.php
@@ -1,62 +1,75 @@
+<div class="form-group col-lg-4">
+    {{ html()->label('Nombre') }}
+    {{ html()->text('nombre')->placeholder('Ingrese el Nombre')->class('form-control')->value(old('nombre', $project_manager->nombre ?? '')) }}
+</div>
+<div class="form-group col-lg-3">
+    {{ html()->label('Centro de Costo') }}
+    {{ html()->text('centro_costo')->placeholder('Ingrese el Centro de Costo')->class('form-control')->value(old('centro_costo', $project_manager->centro_costo ?? '')) }}
+</div>
+<div class="form-group col-lg-3">
+    {{ html()->label('Servicio') }}
+    {{ html()->select('service_id', $servicios)->placeholder('Ingrese el Servicio')->class('form-control select')->value(old('service_id', $project_manager->service_id ?? '')) }}
+</div>
+<div class="form-group col-lg-2">
+    {{ html()->label('Prioridad') }}
+    {{ html()->select('prioridad', $priorities)->placeholder('Ingrese la Prioridad')->class('form-control select')->value(old('prioridad', $project_manager->prioridad ?? '')) }}
+</div>
+<div class="form-group col-lg-6">
+    {{ html()->label('Cliente') }}
+    {{ html()->select('cliente_id', $responsables)->placeholder('Ingrese al Cliente')->class('form-control select client')->value(old('cliente_id', $project_manager->cliente_id ?? '')) }}
+</div>
+<div class="form-group col-lg-6">
+    {{ html()->label('Administrador') }}
+    {{ html()->select('administrador_id', $administradores)->placeholder('Ingrese al Administrador')->class('form-control select')->value(old('administrador_id', $project_manager->administrador_id ?? '')) }}
+</div>
+<div class="form-group col-lg-6">
+    {{ html()->label('Ruc') }}
+    {{ html()->text('ruc')->placeholder('Ingrese el ruc')->class('form-control ruc')->readonly()->value(old('ruc', $project_manager->ruc ?? '')) }}
+</div>
+<div class="form-group col-lg-6">
+    {{ html()->label('Responsable') }}
+    {{ html()->select('responsable_id', $responsables)->placeholder('Ingrese al Responsable')->class('form-control select')->value(old('responsable_id', $project_manager->responsable_id ?? '')) }}
+</div>
+<div class="form-group col-lg-6">
+    {{ html()->label('Fecha de Inicio') }}
+    {{ html()->datetime('fecha_inicio')->placeholder('Ingrese la Fecha de Inicio')->class('form-control datetime')->value(old('fecha_inicio', $project_manager->fecha_inicio ?? '')) }}
+</div>
+<div class="form-group col-lg-6">
+    {{ html()->label('Fecha de Cierre') }}
+    {{ html()->datetime('fecha_cierre')->placeholder('Ingrese la Fecha de Cierre')->class('form-control datetime')->value(old('fecha_cierre', $project_manager->fecha_cierre ?? '')) }}
+</div>
+@push('js')
+    @once
+        <script src="{{ asset('js/plugins/select2/select2.full.min.js') }}"></script>
+        <script src="{{ asset('js/plugins/datapicker/bootstrap-datepicker.js') }}"></script>
+        <script>
+            $(".select").select2({
+                containerCssClass: ':all:'
+            });
+            // $(".datetime").datepicker()
 
-<div class="col-md-6">
-    <div class="form-group d-flex align-items-center"> 
-        {{ html()->label('Nombre')->class('label-custom me-2') }}
-        {{ html()->text('nombre')->placeholder('Ingrese el Nombre')->class('form-control')->value(old('nombre',$project_manager->nombre ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Centro de Costo')->class('fs-5')}}
-        {{html()->text('centro_costo')->placeholder('Ingrese el Centro de Costo')->class('form-control')->value(old('centro_costo',$project_manager->centro_costo ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Ruc')->class('col-form-label')}}
-        {{html()->text('ruc')->placeholder('Ingrese el ruc')->class('form-control')->value(old('ruc',$project_manager->ruc ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex">
-        {{html()->label('Administrador')->class('col-form-label')}}
-        {{html()->select('administrador_id',$administradores)->placeholder('Ingrese al Administrador')->class('form-control')->value(old('administrador_id',$project_manager->administrador_id ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Responsable')->class('col-form-label')}}
-        {{html()->select('responsable_id',$responsables)->placeholder('Ingrese al Responsable')->class('form-control')->value(old('responsable_id',$project_manager->responsable_id ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Servicio')->class('col-form-label')}}
-        {{html()->select('project_service_id',$projectServices)->placeholder('Ingrese el Servicio')->class('form-control')->value(old('project_service_id',$project_manager->project_service_id ?? ''))}}
-    </div>
-</div>
+            $(".client").on("change", (e) => {
+                const select_element = $(e.target)
+                const input_element = $(".ruc")
 
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Cliente')->class('col-form-label')}}
-        {{html()->select('cliente_id',$responsables)->placeholder('Ingrese al Cliente')->class('form-control')->value(old('cliente_id',$project_manager->cliente_id ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Prioridad')->class('col-form-label')}}
-        {{html()->select('prioridad',$priorities)->placeholder('Ingrese la Prioridad')->class('form-control')->value(old('prioridad',$project_manager->prioridad ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Fecha de Inicio')->class('col-form-label')}}
-        {{html()->datetime('fecha_inicio')->placeholder('Ingrese la Fecha de Inicio')->class('form-control')->value(old('fecha_inicio',$project_manager->fecha_inicio ?? ''))}}
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="form-group d-flex"> 
-        {{html()->label('Fecha de Cierre')->class('col-form-label')}}
-        {{html()->datetime('fecha_cierre')->placeholder('Ingrese la Fecha de Cierre')->class('form-control')->value(old('fecha_cierre',$project_manager->fecha_cierre ?? ''))}}
-    </div>
-</div>
+                const id = select_element.val()
+                if (!id) return; // validation id
+                $.ajax({
+                        url: `/cliente/${id}`,
+                        method: 'GET',
+                        headers: {
+                            'Accept': 'application/json'
+                        }
+                    })
+                    .done(function(data) {
+                        input_element.val(data.numero_documento)
+                        toastr.success("Datos del cliente obtenidos correctamente");
+                    })
+                    .fail(function(jqXHR, textStatus, errorThrown) {
+                        console.error('Error:', textStatus, errorThrown);
+                        toastr.error("Error al obtener los datos del cliente"); // Mensaje de error
+                    });
+            })
+        </script>
+    @endonce
+@endpush
