0ea6c9c8773770f94e92101d90c17715152578f2
Fernando Huaman <pedrobecmucha12@gmail.com>
2025-02-15 20:03:32 -0500

    Progreso de vista de clientes

--- resources/views/servicio/vistaclientes.blade.php
+++ resources/views/servicio/vistaclientes.blade.php
@@ -6,41 +6,74 @@
 
 @section('content')
 @include('servicio\_shared\tabs')
-    <style>
 
-    </style>
-    <h2>CLIENTES</h2>
-    <table class="table table-striped table-bordered table-hover dataTables-example">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>NOMBRE</th>
-                <th>APELLIDO</th>
-                <th>DNI</th>
-                <th>CELULAR</th>
-                <th>CORREO</th>
-                <th>ACCIÓN</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>01</td>
-                <td>Carlos Daniel</td>
-                <td>Roman Berru</td>
-                <td>73588510</td>
-                <td>936292675</td>
-                <td>danielrberru@gmail.com</td>
-                <td><button>Ver estado</button></td>
-            </tr>
-            <tr>
-                <td>02</td>
-                <td>Christopher Javier</td>
-                <td>Huaman Guevara</td>
-                <td>74894537</td>
-                <td>934361536</td>
-                <td>christojhg@gmail.com</td>
-                <td><button>Ver estado</button></td>
-            </tr>
-        </tbody>
-    </table>
+<style>
+    .table thead {
+        background-color: white;
+        color: #15338a;
+        text-align: center;
+    }
+
+    .table {
+        border: 2px solid black;
+    }
+
+    .table th, .table td {
+        border: 1px solid black !important;
+        padding: 10px;
+        text-align: center;
+    }
+
+    .table tbody tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    .btn-estado {
+        background-color: #15338a;
+        color: white;
+        border: none;
+        padding: 5px 10px;
+        cursor: pointer;
+        border-radius: 5px;
+    }
+
+    .btn-estado:hover {
+        background-color: black;
+    }
+</style>
+
+<h2>CLIENTES</h2>
+<table class="table table-striped table-bordered table-hover dataTables-example">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>NOMBRE</th>
+            <th>APELLIDO</th>
+            <th>DNI</th>
+            <th>CELULAR</th>
+            <th>CORREO</th>
+            <th>ACCIÓN</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>Carlos Daniel</td>
+            <td>Roman Berru</td>
+            <td>73588510</td>
+            <td>936292675</td>
+            <td>danielrberru@gmail.com</td>
+            <td><button class="btn-estado">Ver acciones</button></td>
+        </tr>
+        <tr>
+            <td>02</td>
+            <td>Christopher Javier</td>
+            <td>Huaman Guevara</td>
+            <td>74894537</td>
+            <td>934361536</td>
+            <td>christojhg@gmail.com</td>
+            <td><button class="btn-estado">Ver acciones</button></td>
+        </tr>
+    </tbody>
+</table>
 @endsection
