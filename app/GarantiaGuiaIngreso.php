48675dbfdbabb0ef7cd33ba00681fd39deb2d73b
pepo321 <pedrobecmucha12@gmail.com>
2025-02-22 19:55:57 -0500

    Antes modificacion a nuevas vistas

--- app/GarantiaGuiaIngreso.php
+++ app/GarantiaGuiaIngreso.php
@@ -9,25 +9,12 @@ class GarantiaGuiaIngreso extends Model
     protected $table = 'garantia_guia_ingreso';
 
     protected $fillable = [
-        'motivo',
-        'fecha',
-        'orden_servicio',
-        'estado',
-        'egresado',
-        'asunto',
-        'nombre_equipo',
-        'numero_serie',
-        'codigo_interno',
-        'fecha_compra',
-        'descripcion_problema',
-        'revision_diagnostico',
-        'estetica',
-        'marca_id',
-        'personal_lab_id',
-        'cliente_id',
-        'contacto_cliente_id',
+        'motivo', 'fecha', 'orden_servicio', 'estado', 'egresado', 'asunto',
+        'nombre_equipo', 'numero_serie', 'codigo_interno', 'fecha_compra',
+        'descripcion_problema', 'revision_diagnostico', 'estetica', 'marca_id',
+        'contacto_cliente_id', 'created_at', 'updated_at'
     ];
-    
+
     protected $guarded = [];
 
     public function marcas_i(){
@@ -45,6 +32,5 @@ class GarantiaGuiaIngreso extends Model
     public function contactos(){
         return $this->belongsTo(Contacto::class,'contacto_cliente_id');
     }
-
-
+    
 }
