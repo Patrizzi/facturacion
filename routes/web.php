a69af3949e2c35aecbdd50038a114061b27552d1
Coolmesito <msamaniegocalderon@gmail.com>
2025-02-25 16:41:53 -0500

    Merge Fabricio

--- routes/web.php
+++ routes/web.php
@@ -416,7 +416,7 @@ Route::group(
 		Route::resource('/kardex-entrada-Distribucion','KardexEntradaDistribucionController');
 		Route::get('/kardex_distribucion_guia_print/{id}','KardexEntradaDistribucionController@print')->name('kardex-distribucion.print');
 		Route::resource('/kardex-entrada','KardexEntradaController');
-		// Route::post('/kardex-entrada/destroy','KardexEntradaController@destroy')->name('kardex-entrada.destroy');
+		Route::post('/kardex-entrada/destroy','KardexEntradaController@destroy')->name('kardex-entrada.destroy');
 		Route::post('/kardex-entrada/inventario-inicial','KardexEntradaController@InventarioInicial')->name('kardex-entrada.i_inicial');
 
 
