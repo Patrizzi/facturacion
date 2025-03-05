4fa94cfbca842f3ff05a87fec8633ecc8676e48c
Joaquin <jjacvargas@gmail.com>
2025-03-05 10:27:22 -0500

    guardado 2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -462,12 +462,29 @@
                     @endcan
 
                     <li>
-                        <a href="{{ route('clientes.index') }}">
+                       <a href="{{ route('clientes.index') }}">
                             <img src="{{ asset('/archivos/imagenes/layout/servicio_tecnico.png') }}" class="iconos">
                             <span class="nav-label">Servicio Técnico</span>
                         </a>
-                    </li>
 
+                         {{-- <a href="#"><img src="{{ asset('/archivos/imagenes/layout/servicio_tecnico.png') }}"
+                            class="iconos"> <span class="nav-label">Servicio Técnico</span></a>
+                    <ul class="nav nav-second-level collapse">
+                        @can('transacciones-garantias-guias_ingreso.index')
+                            <li><a href="{{ route('garantia_guia_ingreso.index') }}"><span>Guía Ingreso</span></a>
+                            </li>
+                        @endcan
+                        @can('transacciones-garantias-guias_egreso.index')
+                            <li><a href="{{ route('garantia_guia_egreso.index') }}"><span>Guía Egreso</span></a></li>
+                        @endcan
+                        @can('transacciones-garantias-informe_tecnico.index')
+                            <li><a href="{{ route('garantia_informe_tecnico.index') }}"><span>Informe
+                                        Técnico</span></a></li>
+                        @endcan
+
+                    </ul> --}}
+
+                    </li>
 
                     <li>
                         @if (empty($inventario_inicial))
