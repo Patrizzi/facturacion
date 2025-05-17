54f3696047222f38ace3cfbfe6b42d8df19270c8
Somies <misaelsoria789@gmail.com>
2025-05-17 17:25:52 -0500

    extraer cambios pedro

--- resources/views/producto_servicios/productos/index.blade.php
+++ resources/views/producto_servicios/productos/index.blade.php
@@ -14,11 +14,28 @@
     </div>
     <button type="submit" class="btn btn-primary">Subir</button>
 </form>
-@if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+        </ul>
     </div>
 @endif
+
+@if(session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
+
+@if(session('warning'))
+    <div class="alert alert-warning">
+        {!! session('warning') !!}
+    </div>
+@endif
+
 
 
 
