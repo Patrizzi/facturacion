6caa610037da5b54f61434ee26bf857d6081bf60
Somies <misaelsoria789@gmail.com>
2025-05-18 18:58:31 -0500

    guaardaar progreso del excel 170525

--- app/Subfamilia.php
+++ app/Subfamilia.php
@@ -6,7 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subfamilia extends Model
 {
-   protected $fillable = [
-        'id_familia', 'codigo', 'descripcion', 'ubicacion', 'estado'
-    ];
 }
