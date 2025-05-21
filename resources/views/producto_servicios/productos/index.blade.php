eb6af7d23190194a479376e66f54a8ca68378ff3
pepo321 <pedrobecmucha12@gmail.com>
2025-05-21 09:45:34 -0500

    guardar datos

--- resources/views/producto_servicios/productos/index.blade.php
+++ resources/views/producto_servicios/productos/index.blade.php
@@ -5,43 +5,6 @@
 @section('href_accion', route('productos.create'))
 @section('content')
 
-<<<<<<< HEAD
-<div class="wrapper wrapper-content animated fadeInRight">
-<form action="{{ route('productos.importar') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div class="form-group">
-        <label for="excel">Subir archivo Excel:</label>
-        <input type="file" name="excel" id="excel" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Subir</button>
-</form>
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-        </ul>
-    </div>
-@endif
-
-@if(session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
-    </div>
-@endif
-
-@if(session('warning'))
-    <div class="alert alert-warning">
-        {!! session('warning') !!}
-    </div>
-@endif
-@if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
-=======
     <div class="wrapper wrapper-content animated fadeInRight">
     <form action="{{ route('productos.importar') }}" method="POST" enctype="multipart/form-data">
         @csrf
@@ -78,7 +41,6 @@
         </div>
     @endif
 
->>>>>>> e59afdf6a7e343fb8571fe270d05f883ebb721c5
 
 
 
