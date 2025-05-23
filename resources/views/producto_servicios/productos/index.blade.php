d0272c9b54558abe2cd0c79ab7356b00aea26086
Coolmesito <msamaniegocalderon@gmail.com>
2025-05-23 01:40:16 -0500

    Avance de index productos

--- resources/views/producto_servicios/productos/index.blade.php
+++ resources/views/producto_servicios/productos/index.blade.php
@@ -10,6 +10,77 @@
             {{ session('anulacion') }}
         </div>
     @endif
+<div class="wrapper wrapper-content animated fadeInRight">
+  <div class="ibox">
+    <div class="ibox-content">
+      <!-- Encabezado -->
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <div class="nav nav-tabs border-0">
+          <a href="#" class="btn btn-link text-dark font-weight-bold">Productos</a>
+          <a href="#" class="btn btn-link text-muted">Paquetes</a>
+          <a href="#" class="btn btn-link text-muted">Familia</a>
+          <a href="#" class="btn btn-link text-muted">Subfamilia</a>
+        </div>
+        <div>
+          <button class="btn btn-primary">Nuevo Producto</button>
+        <i class="fa fa-plus text-secondary mx-2" style="cursor: pointer;"></i>
+        <i class="fa fa-upload text-secondary mx-2" style="cursor: pointer;"></i>
+        <i class="fa fa-download text-secondary mx-2" style="cursor: pointer;"></i>
+        <i class="fa fa-user text-secondary mx-2" style="cursor: pointer;"></i>
+        </div>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-striped">
+            <tr>
+              <th><input type="radio" ></th>
+              <th>Código <i class="fa fa-search"></i></th>
+              <th>Nombre <i class="fa fa-search"></i></th>
+              <th>Marca <i class="fa fa-search"></i></th>
+              <th>Unidad <i class="fa fa-filter"></i></th>
+              <th>Precio <i class="fa fa-search"></i></th>
+              <th>Stock <i class="fa fa-search"></i></th>
+              <th><i class="fa fa-sliders"></i></th>
+            </tr>
+          <tbody>
+            <tr>
+              <td><input type="radio" name="product"></td>
+              <td>LN-000001</td>
+              <td>Laptop Asus TUF Gaming</td>
+              <td>ASUS</td>
+              <td>UNIDAD</td>
+              <td>S/. 2800.00</td>
+              <td>10</td>
+              <td><i class="fa fa-ellipsis-v"></i></td>
+            </tr>
+            <tr>
+              <td><input type="radio" name="product"></td>
+              <td>LN-000002</td>
+              <td>TECLADO INALÁMBRICO</td>
+              <td>LENOVO</td>
+              <td>BOLSA</td>
+              <td>S/. 140.50</td>
+              <td>20</td>
+              <td><i class="fa fa-ellipsis-v"></i></td>
+            </tr>
+            <tr>
+              <td><input type="radio" name="product"></td>
+              <td>ID-000001</td>
+              <td>CABLE IDECO THW</td>
+              <td>IDECO</td>
+              <td>PAQUETE</td>
+              <td>S/. 20.00</td>
+              <td>5</td>
+              <td><i class="fa fa-ellipsis-v"></i></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 
     <!--Código actual 14/11/2024-->
     @include('producto_servicios.shared.stadistics')
