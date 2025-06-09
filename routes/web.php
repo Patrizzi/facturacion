a0383e4a52c27a420d17ba35970e234095fdfb0c
JYPSAC <julioflores@jypsac.com>
2025-06-09 14:31:34 -0500

    resolve create guia remision

--- routes/web.php
+++ routes/web.php
@@ -251,9 +251,9 @@ Route::group(
 		//para guia agregar el store en create_moneda secundaria enviando este una acptacion de 2 variables put en store para la identificaion de la moneda principal o secundaria
 		Route::get('/guia_remision/print/{id}' , 'GuiaRemisionController@print')->name('guia_remision.print');
 
-		Route::resource('/guia_remision','GuiaRemisionController');
+		Route::resource('/guia_remision','GuiaRemisionController')->except(['create']);
 		Route::post('/guia_remision/sucursal','GuiaRemisionController@ajax_sucursal')->name('guia_remision.ajax_sucursal');
-		// Route::post('/guia_remision/create','GuiaRemisionController@create')->name('guia_remision.create');
+		Route::post('/guia_remision/create','GuiaRemisionController@create')->name('guia_remision.create');
 		// Route::post('/guia_remision/ajax_p','GuiaRemisionController@ajax_producto')->name('remision.ajax_producto');
 		Route::post('/guia_remision/peso_stock','GuiaRemisionController@peso_stock')->name('guia_remision.peso_stock');
 		/* REMISION MANUAL */
