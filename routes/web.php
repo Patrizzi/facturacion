7c1ef2f9f473e4643c745a1c69d7be356c1f2622
JYPSAC <julioflores@jypsac.com>
2025-06-13 10:26:53 -0500

    clientes

--- routes/web.php
+++ routes/web.php
@@ -670,7 +670,7 @@ Route::group(
 		Route::get('/ventas/cotizacion', 'CotizacionController@index3')->name('ventas.cotizacion');
 		Route::get('/ventas/cotizacion_manual', 'CotizacionManualController@index2')->name('ventas.cotizacion_manual');
 		Route::get('/ventas/nota_venta', 'NotaVentaController@index2')->name('ventas.nota_venta');
-		Route::get('/ventas/clientes', 'ClienteController@ventas_index')->name('ventas.clientes');
+		Route::get('/ventas/clientes', 'ClienteController@ventas_indexphp ar')->name('ventas.clientes');
 		// Los registros están en cada seccion de cotizacion
 
 		Route::get('/creditos', 'CreditosAdelantosController@creditos')->name('cobranzas.creditos');
