8968ecbdbce04f6a8d75ca90ff6b644cf50336c8
JYPSAC <julioflores@jypsac.com>
2025-07-15 16:07:14 -0500

    nota debito correccion

--- app/Nota_Debito.php
+++ app/Nota_Debito.php
@@ -15,13 +15,13 @@ class Nota_Debito extends Model
     }  
 
     public function nota_i_fac_manual(){
-        return $this->belongsTo(Facturacion::class,'facturacion_m_id');
+        return $this->belongsTo(Facturacion_m::class,'facturacion_m_id');
     }  
 
     public function nota_i_boleta(){
         return $this->belongsTo(Boleta::class,'boleta_id');
     } 
     public function nota_i_boleta_manual(){
-        return $this->belongsTo(Facturacion::class,'boleta_m_id');
+        return $this->belongsTo(Boleta_m::class,'boleta_m_id');
     }  
 }
