f310d5d8be709247987a040de1c57c9087218d4e
JYPSAC <julioflores@jypsac.com>
2025-07-30 15:45:43 -0500

    ??

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+        // Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
