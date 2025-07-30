bb4950f83eaea22a7a1b4485b3638596557397bd
JYPSAC <julioflores@jypsac.com>
2025-07-30 18:10:35 -0500

    resolve vendedor

--- app/Http/Controllers/BoletaController.php
+++ app/Http/Controllers/BoletaController.php
@@ -576,7 +576,7 @@ return view('transaccion.venta.boleta.create_ms',compact('productos','forma_pago
         $comisionista_porcentaje=Personal_venta::where('id',$comision_id)->first();
         $comisionista=new Ventas_registro;
         $comisionista->comisionista=$comision_id;
-        $comisionista->tipo_moneda=$id_moneda;
+        $comisionista->tipo_moneda=$id_moneda->id;
         $comisionista->estado_aprobado='0';
         $comisionista->estado_pagado='0';
         $comisionista->estado_anular_fac_bol='0';
