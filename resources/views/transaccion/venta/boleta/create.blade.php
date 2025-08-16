28e6dcb179921db896f724d3c15b26f391fad784
PedroBM29-12 <pedrobecmucha12@gmail.com>
2025-08-16 17:11:49 -0500

    correcion y cambio del boton type button a submit, en boleta_create

--- resources/views/transaccion/venta/boleta/create.blade.php
+++ resources/views/transaccion/venta/boleta/create.blade.php
@@ -364,7 +364,7 @@
                         </div>
                         <div class="col-md-12">
                             <div class="d-flex justify-content-end mt-4">
-                                <button type="button" id="boton" name="boton" class="btn btn-primary button-lada"
+                                <button type="submit" id="boton" name="boton" class="btn btn-primary button-lada"
                                     style="background: #0400c2; border-radius:8px; font-weight:450; font-size: 1rem; padding: 7px 20px;">
                                     <strong>Guardar</strong>
                                 </button>
