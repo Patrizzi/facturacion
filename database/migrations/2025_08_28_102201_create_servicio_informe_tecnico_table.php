f9f9bcddefb8bad13d62899a78ac59f9fbd77707
PedroBM29-12 <pedrobecmucha12@gmail.com>
2025-08-28 11:02:32 -0500

    base de datos agregado para servicio tecnico

--- database/migrations/2025_08_28_102201_create_servicio_informe_tecnico_table.php
+++ database/migrations/2025_08_28_102201_create_servicio_informe_tecnico_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSGuiaSalidaTable extends Migration
+class CreateServicioInformeTecnicoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateSGuiaSalidaTable extends Migration
      */
     public function up()
     {
-        Schema::create('s_guia_salida', function (Blueprint $table) {
+        Schema::create('servicio_informe_tecnico', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('s_guia_id');
-            $table->foreign('s_guia_id')->references('id')->on('s_guias')->onDelete('cascade');
-            $table->boolean('terminado')->default(false);
+            $table->unsignedBigInteger('servicio_g_egreso_id');
+            $table->foreign('servicio_g_egreso_id')->references('id')->on('servicio_g_egresos');
+            $table->date('fecha_creacion');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateSGuiaSalidaTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('s_guia_salida');
+        Schema::dropIfExists('servicio_informe_tecnico');
     }
 }
--- database/migrations/2025_08_28_104115_create_servicio_imagenes_table.php
+++ database/migrations/2025_08_28_104115_create_servicio_imagenes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSGuiaIngresoTable extends Migration
+class CreateServicioImagenesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateSGuiaIngresoTable extends Migration
      */
     public function up()
     {
-        Schema::create('s_guia_ingreso', function (Blueprint $table) {
+        Schema::create('servicio_imagenes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('s_guia_id');
-            $table->foreign('s_guia_id')->references('id')->on('s_guias')->onDelete('cascade');
+            $table->string('imagen');
+            $table->unsignedBigInteger('servicio_g_egreso_id');
+            $table->foreign('servicio_g_egreso_id')->references('id')->on('servicio_g_egresos');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateSGuiaIngresoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('s_guia_ingreso');
+        Schema::dropIfExists('servicio_imagenes');
     }
 }
