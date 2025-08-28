0237e75cbadc7eb133385b68b6f66735e7294760
PedroBM29-12 <pedrobecmucha12@gmail.com>
2025-08-28 10:15:35 -0500

    eliminacion de las tablas servicio tecnico

---
+++ app/Http/Controllers/ServicioTecnico/ServicioGuiaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\ServicioTecnico;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ServicioGuiaController extends Controller
+{
+    public function index() {
+
+    }
+
+    public function store() {
+        
+    }
+}
