64e5413cfc4d348c203d33eb5e5df0d1cc090303
Renzo <renzoperez5000@gmail.com>
2025-08-29 12:36:12 -0500

    falta logica proceso servicio guia

--- app/Http/Controllers/ServicioTecnico/ProcesoServicioGuiaController.php
+++ app/Http/Controllers/ServicioTecnico/ProcesoServicioGuiaController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\ServicioTecnico;
 
 use App\Http\Controllers\Controller;
+use App\ServicioGuia;
+use App\ServicioGuiaIngreso;
+use Exception;
 use Illuminate\Http\Request;
 
 class ProcesoServicioGuiaController extends Controller
@@ -11,7 +14,21 @@ class ProcesoServicioGuiaController extends Controller
 
     }
 
-    public function store() {
+    public function store(Request $request) {
+        try {
 
+            $servicioGuia = ServicioGuia::findOrFail($request->servicio_guia_id);
+
+            if($servicioGuia->estado == 2) {
+                return redirect()->route('servicio-guias.proceso', $servicioGuia->id)->with('warning', 'Este servicio ya ha sido cotizado. No se puede agregar más equipos.');
+            }
+
+            ServicioGuiaIngreso::create([
+                'servicio_guia_id' => $servicioGuia->id,
+            ]);
+
+        } catch(Exception $e) {
+
+        }
     }
 }
