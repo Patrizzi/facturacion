59e902387f5dd30f83c59d71bfee3b3a1d9e2ad4
pepo321 <pedrobecmucha12@gmail.com>
2025-09-03 10:50:42 -0500

    vista print multiple solucionado

--- resources/views/transaccion/garantias/informe_tecnico/print_multiple.blade.php
+++ resources/views/transaccion/garantias/informe_tecnico/print_multiple.blade.php
@@ -26,7 +26,7 @@
     @foreach($informesData as $index => $informeData)
         @php
             $garantias_informe_tecnico = $informeData['informe'];
-            $archivo_informe_tecnico = $informeData['archivos'];
+            // $archivo_informe_tecnico = $informeData['archivos'];
             $usuario = $informeData['usuario'];
         @endphp
 
@@ -171,14 +171,7 @@
                                         <div class="form-control" style="height: 100%">
                                             <h3>Imagenes</h3>
                                             <div align="left" style="font-size: 13px">
-                                                <div class="row" align="center">
-                                                    @foreach($archivo_informe_tecnico as $archivo)
-                                                    <div class="col-sm-4" style="padding: 50px;padding-left: 70px ;margin: -15px" align="center">
-                                                        <img src="{{asset('archivos/imagenes/informe_tecnico')}}/{{$archivo->archivos}}" style="width:270px;height: 270px;border-radius: 10px">
-                                                        <br>
-                                                    </div>
-                                                    @endforeach
-                                                </div>
+
                                             </div>
                                         </div>
                                     </div>
