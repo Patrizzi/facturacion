8133355186fad1204f1036ed2db8b5af8ded7466
pedrobm2912 <pedrobecmucha12@gmail.com>
2025-10-05 20:17:03 -0500

    correciones pdf

--- resources/views/transaccion/comprobantes/boleta/index.blade.php
+++ resources/views/transaccion/comprobantes/boleta/index.blade.php
@@ -79,10 +79,12 @@
                                             title="Exportar a Excel">
                                             <i class="fa fa-upload"></i>
                                         </button>
-                                        <button type="button" id="btn-descargar-filtrado" class="btn btn-primary"
+
+                                        {{-- aun no funcional --}}
+                                        {{-- <button type="button" id="btn-descargar-filtrado" class="btn btn-primary"
                                             title="Descargar a PDF zip">
                                             <i class="fa fa-download"></i>
-                                        </button>
+                                        </button> --}}
                                     </ul>
                                 </ul>
                             </div>
