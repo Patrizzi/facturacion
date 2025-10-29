03867a4b18e916659ee203b8bec0b98391f3606f
ZER0SEV7N <danielenriquesinger0@gmail.com>
2025-10-29 12:09:06 -0500

    renovaciones

--- resources/views/transaccion/venta/renovacion/index.blade.php
+++ resources/views/transaccion/venta/renovacion/index.blade.php
@@ -1,18 +1,27 @@
 @extends('layout')
 
+@section('title', 'Ventas | Renovaciones')
+
 
 @section('content')
     <div class="wrapper wrapper-content animated fadeInRight">
-        
+
         {{-- RESUMEN (si lo necesitas) --}}
         <div class="row">
             <div class="col-lg-12">
                 <div class="ibox">
                     <div class="ibox-title">
                         <h4>Resumen de {{ Str::ucfirst(Carbon\Carbon::now()->translatedFormat('F Y')) }}</h4>
+                        <div class="ibox-tools custom">
+                            <a class="collapse-link">
+                                <i class="fa fa-chevron-up"></i>
+                            </a>
+                        </div>
                     </div>
                     <div class="ibox-content">
-                        {{-- Tu resumen aquí --}}
+                        <div class="row">
+                           {{--   @include('transaccion\venta\_shared\statistics')--}}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -23,25 +32,112 @@
             <div class="col-lg-12">
                 <div class="ibox">
                     <div class="ibox-content">
-                        
-                        {{-- TABS --}}
-                        <ul class="nav nav-tabs" role="tablist">
-                            @include('transaccion\venta\_shared\tabs')
-                        </ul>
+                        <div class="tabs-scroll-top"></div>
+                        <div class="tabs-scroll-bottom">
+                            {{-- TABS --}}
+                            <ul class="nav nav-tabs" role="tablist" style="align-items: center;border-bottom: 0px !important;">
+                                @include('transaccion\venta\_shared\tabs')
+                                {{-- Almacen --}}
+                                <ul class="ml-auto d-flex"
+                                    style="gap: 10px; align-items: center;z-index: 20;position: fixed;right: 40px">
+                                    <ul class="ml-auto d-flex" style="gap: 10px; align-items: center;">
+                                        <a class="btn btn-primary" href="{{ route('cotizacion_manual.create') }}"><i
+                                                class="fa fa-plus"></i>
+                                        </a>
+                                    </ul>
+                                    {{-- btn duplicar --}}
+                                    {{--  <a href="#" id="btn-duplicar-cotizacion" class="btn btn-primary" title="Duplicar Cotización">
+                                        <i class="fa fa-copy"></i>
+                                    </a>--}}
 
-                        {{-- CONTENIDO DE LOS TABS --}}
-                        <div class="tab-content">
-                            <div role="tabpanel" id="tab-1" class="tab-pane active">
-                                <br>
-                                {{-- Aquí va tu contenido --}}
-                                <div class="table-responsive">
-                                    <table class="table">
-                                        {{-- Tu tabla --}}
-                                    </table>
+                                    <button type="button" id="bnt-imprimir" class="btn btn-primary" title="Imprimir">
+                                        <i class="fa fa-print"></i>
+                                    </button>
+                                    <button type="button" id="btn_export_renovacion" class="btn btn-primary" title="Exportar a Excel">
+                                        <i class="fa fa-upload"></i>
+                                    </button>
+
+                                    <button type="button" id="btn-descargar-filtrado" class="btn btn-primary"
+                                        title="Descargar a PDF zip">
+                                        <i class="fa fa-download"></i>
+                                    </button>
+                                </ul>
+                            </ul>
+                        </div>
+                        <div class="tab-content" style="margin-top: -1px">
+                                <!-- COTIZACION MANUAL-->
+                                <div role="tabpanel" id="tab-2" class="tab-pane active show" style="margin-top: -1px;border-top: 1px solid #e7eaec !important;">
+                                    <br> {{-- FILTRADO DE DATOS --}}
+                                    <div class="search-responsive">
+                                        <div class="row" style="row-gap: 5px">
+                                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                                <div class="input-group">
+                                                    <input class="form-control" type="text" name="daterange"
+                                                        id="data_range_filter"
+                                                        value="{{ date('01/m/Y') }} - {{ date('t/m/Y') }}"
+                                                        readonly="readonly" />
+                                                    <span class="input-group-append">
+                                                        <button type="button" class="btn btn-secondary" id="revert_select">
+                                                            <i class="fa fa-history"></i>
+                                                        </button>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3 col-md-6 col-sm-12">
+                                                <select class="form-control" name="" id="select_tipo_coti">
+                                                    <option value="" selected>Todos los comprobantes</option>
+                                                    <option value="factura">Factura</option>
+                                                    <option value="boleta">Boleta</option>
+                                                    <option value="nota_venta">Nota de Venta</option>
+                                                </select>
+                                            </div>
+                                            <div class="col-lg-3 col-md-6 col-sm-12">
+                                                <input type="search" class="form-control" placeholder="Buscar:"
+                                                    id="search_all_column">
+                                            </div>
+                                            <div class="col-lg-2 col-md-6 col-sm-12">
+                                                <button type="button" class="btn btn-block btn-primary"
+                                                    id="filter_buttons">Buscar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>{{--  Tabla de Cotizacion Manual   --}}
+                                    <div class="scrooll-table-responsive">
+                                    </div>
+                                    <div class="table-responsive">
+                                        <table
+                                            class="table table-striped table-bordered dataTables-example-cotizacion_manual" style="min-width: 982px">
+                                            <thead>
+                                                <tr>
+                                                    <th>
+                                                        <input type="checkbox" class="i-checks" name="input[]">
+                                                    </th>
+                                                    <th>ID</th>
+                                                    <th>N°</th>
+                                                    <th>RUC-DNI</th>
+                                                    <th>Cliente</th>
+                                                    <th>Fecha de Emisión</th>
+                                                    <th>Fecha de fin</th>
+                                                    <th>Forma</th>
+                                                    <th>Importe T.</th>
+                                                    <th style="width: 0.5vmax !important">Acciones</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                            </tbody>
+                                            <tfoot>
+                                                <tr>
+                                                    <th colspan="7"></th>
+                                                    <th class="total-columna">Total: 0</th>
+                                                    <th class="total-total">Total G: 0</th>
+                                                </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
             </div>
@@ -58,5 +154,18 @@
     <script src="{{ asset('js/plugins/dataTables/dataTables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('js/inspinia.js') }}"></script>
     <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/fullcalendar/moment.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <!-- Custom and plugin javascript -->
+    <script src="{{ asset('js/inspinia.js') }}"></script>
+    <script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
+
+    <!-- check -->
+    <script src="{{ asset('js/plugins/iCheck/icheck.min.js') }}"></script>
+    <script src="{{ asset('js/icheck.min.js') }}"></script>
+
+    <script src="{{ asset('js/plugins/sweetalert/sweetalert.min.js') }}"></script>
+
+    @include('transaccion.venta._shared.js_shared')
 
 @endsection
