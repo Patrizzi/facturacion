f5f1860556ecb8ef23a64946a121a6995e67f0c1
JYPSAC <julioflores@jypsac.com>
2026-01-14 09:23:26 -0500

    Remision normal y manual print multiple anuladas

--- resources/views/transaccion/comprobantes/guia_remision/print_multiple.blade.php
+++ resources/views/transaccion/comprobantes/guia_remision/print_multiple.blade.php
@@ -186,6 +186,25 @@
         page-break-after: auto;
       }
     }
+    #watermark {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 0;
+    }
+    #watermark p {
+      position: absolute;
+      color: rgba(120, 120, 120, 0.31);
+      font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif !important;
+      font-weight: bolder;
+      font-size: 95px !important;
+      pointer-events: none;
+      -webkit-transform: rotate(-45deg);
+      -moz-transform: rotate(-45deg);
+      top: 35%;
+      right: 35%;
+      z-index: 0;
+    }
   </style>
 
     <script>
@@ -243,7 +262,11 @@
                 <div class="serie">N°: {{ $g->cod_guia }}</div>
             </div>
         </div>
-
+        @if ($g->g_electronica == 2 || $g->estado_anulado == 1)
+            <div id="watermark">
+                <p>Anulado</p>
+            </div>
+        @endif
         <div class="grid-2">
             <div class="box">
                 <div class="box-title">PUNTO DE PARTIDA</div>
