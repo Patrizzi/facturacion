b4d8a92a5865d579feecd3d95ebf7d3378fa6ba9
JYPSAC <julioflores@jypsac.com>
2026-02-17 10:19:11 -0500

    resolve guia manual descarga cdr

--- resources/views/facturacion_electronica/guia_remision/enviado_manual.blade.php
+++ resources/views/facturacion_electronica/guia_remision/enviado_manual.blade.php
@@ -287,7 +287,7 @@
 
                                 var finish_all = `
                                 <div id="div_btn_app_man">
-                                    <button type="button" class="btn" id="guia_remi_ind_man" value="${full[2]}" onclick="valid_cdr_normal(this)"><img src="{{ asset('cdr.png') }}" width="25px"></button>
+                                    <button type="button" class="btn" id="guia_remi_ind_man" value="${full[2]}" onclick="valid_cdr_manual(this)"><img src="{{ asset('cdr.png') }}" width="25px"></button>
                                 </div>
                                 <div style="display: none;" id="div_dw_non_man">
                                     <a id="download_cdr_post" href="${url}" download ><img src="{{ asset('cdr.png') }}" width="25px"></a>   
