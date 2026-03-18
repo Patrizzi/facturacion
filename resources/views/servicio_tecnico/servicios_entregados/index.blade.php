0dbbc9b5709d0390e652a0a83d4f3b5609f7802f
ZER0SEV7N <danielenriquesinger0@gmail.com>
2026-03-18 12:27:43 -0500

    Servicio Infor Tecn show

--- resources/views/servicio_tecnico/servicios_entregados/index.blade.php
+++ resources/views/servicio_tecnico/servicios_entregados/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('title', 'Servicio Técnico Entregados')
-@section('href_accion', route('servicio.index'))
+@section('href_accion', route('servicio-guias.index'))
 @section('value_accion', 'Atras')
 @section('atributo_actu', 'hidden')
 
