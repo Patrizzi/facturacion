06af1cf17185c6847058388f76d3d275f61fa5c2
JYPSAC <julioflores@jypsac.com>
2026-05-05 12:35:55 -0500

    Comision Resolve

--- app/Personal_venta.php
+++ app/Personal_venta.php
@@ -13,4 +13,22 @@ class Personal_venta extends Model
     public function personal(){
         return $this->belongsTo(Personal_datos_laborales::class,'id_personal');
     }
+
+    public function getComisionAttribute(){
+        $value = $this->attributes['comision'];
+        if (is_null($value) || $value === '') {
+            return 0; // o 0 si prefieres
+        }
+
+        // Normalizar (por si viene con espacios tipo " 5 % ")
+        $value = trim($value);
+
+        // Quitar % solo si existe
+        if (str_contains($value, '%')) {
+            $value = str_replace('%', '', $value);
+        }
+
+        return (float) $value;
+    }
+
 }
