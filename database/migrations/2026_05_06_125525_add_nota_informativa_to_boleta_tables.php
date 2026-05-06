f713e217a43fb5409f9bd8bb3a56f3e7a2223922
YossmelBruno <yossmelbrunoamado@gmail.com>
2026-05-06 12:21:19 -0600

    Columna nota informativa añadida a boleta y boleta_m

---
+++ database/migrations/2026_05_06_125525_add_nota_informativa_to_boleta_tables.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNotaInformativaToBoletaTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('boleta', function (Blueprint $table) {
+            $table->text('nota_informativa')->nullable()->after('observacion');
+        });
+
+        Schema::table('boleta_m', function (Blueprint $table) {
+            $table->text('nota_informativa')->nullable()->after('observacion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('boleta', function (Blueprint $table) {
+            $table->dropColumn('nota_informativa');
+        });
+
+        Schema::table('boleta_m', function (Blueprint $table) {
+            $table->dropColumn('nota_informativa');
+        });
+    }
+}
