03d825319f3ae1521b8400c3ea36006f991e06eb
lolo987x <alvaro987x@gmail.com>
2026-09-18 11:54:24 -0500

    Primer commit en la rama patrick  prueba

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,7 @@ class UsersTableSeeder extends Seeder
            'name' => 'Administrador',
            'email' => 'desarrollo@jypsac.com',
            'personal_id' => '1',
-           'password' => bcrypt('@G^e^Fet&VGTsUBqLekW'),
+           'password' => bcrypt('@G^e^Fet&VGTsUBqLekW'),    /** CONTRASEÑA */
             'estado' => 1,
             'confi_id' => 1,
            'email_creado' => 0,
